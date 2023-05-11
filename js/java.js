ff7c25f41bfec68e8a686a1d36f993c504bb299c
Thiago Felipe Guimarães oliveira <115421991+Thiagofgoliver@users.noreply.github.com>
2023-05-11 16:15:24 -0300

    atualizado 11/05/2023

---
+++ js/java.js
@@ -0,0 +1,6 @@
+var btn = document.querySelector('.botao-fechar-cookies');
+var div = document.querySelector('.aviso-cookies');
+
+btn.addEventListener('click', function(){
+    div.style.display = 'none';
+});
